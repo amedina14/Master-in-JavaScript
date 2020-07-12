cfb0efb39b66b6b9af7de2669c4b682006cef286
Adrian Medina <medinalarry96@gmail.com>
2020-07-13 01:59:26 +0200

    data types

---
+++ hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("El resultado de la operacion es: "+operacion);
+
+//Tipo de datos
+var numero_entero = 44;
+var cadena_texto = 'Hola "mundo"';
+var true_false = false;
+
+var numero_falso = "33";
+
+console.log(cadena_texto);
+console.log(true_false);
+
+console.log(Number(numero_falso)+7);
+console.log(parseFloat(numero_falso)+7.5);
+console.log(String(numero_falso)+7);
+
