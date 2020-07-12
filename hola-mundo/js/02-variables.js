591e3cdb03473772926243c67f7b910bfc3dc9d8
Adrian Medina <medinalarry96@gmail.com>
2020-07-13 00:46:59 +0200

    variables, comment, alert, console

---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,11 @@
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 722;
+
+pais= "Mexico";
+continente = "America";
+
+var pais_y_continente = pais+', '+continente;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
