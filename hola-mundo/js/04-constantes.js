851c3aeafaf3e36d78f29c68b063f6df1d657bc8
Adrian Medina <medinalarry96@gmail.com>
2020-07-13 01:28:28 +0200

    constant

---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var web = "https://arandom.000webhostapp.com";
+const ip = "127.0.0.1";
+
+web = "https://arandom.000webhostapp.com/learning-Bootstrap/components.html";
+
+console.log(web, ip);
