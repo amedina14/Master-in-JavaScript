9f89b77fa2b6a4269969f8da1d87e875ebedda18
Adrian Medina <medinalarry96@gmail.com>
2020-07-15 14:28:06 +0200

    Break while

--- hola-mundo/js/09-while.js
+++ hola-mundo/js/09-while.js
@@ -4,15 +4,23 @@
 
 var year = 2020;
 
-while(year >= 2010){
-    console.log("Estamos en el año: "+year--);
+while (year >= 1991) {
+    // esegue questo
+    console.log("Estamos en el año: " + year);
+
+    // BREAK
+    if (year == 1995) {
+        break;
+    }
+
+    year--;
 }
 
 // Do while
 
 var age = 30;
 
-do{
+do {
     alert("only age major than 25");
     age -= 1;
-}while(age >= 25)
+} while (age > 25)
