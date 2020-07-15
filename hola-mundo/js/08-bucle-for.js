409480ca96857ac2758dcbd1874fb4c1a6441d24
Adrian Medina <medinalarry96@gmail.com>
2020-07-15 13:43:51 +0200

    for

---
+++ hola-mundo/js/08-bucle-for.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Bucle for: control structure that repeat n time.
+var numeros = 0;
+var tope = 10;
+
+for (var i = 0; i <= tope; i++) {
+    console.log("Vamos por el numero: " + numeros++);
+
+//    debugger;
+}
