f5c9db734987a8699df4b485b4fa2d29a2e00cce
Adrian Medina <medinalarry96@gmail.com>
2020-07-15 14:21:15 +0200

    do while

--- hola-mundo/js/09-while.js
+++ hola-mundo/js/09-while.js
@@ -7,3 +7,12 @@ var year = 2020;
 while(year >= 2010){
     console.log("Estamos en el año: "+year--);
 }
+
+// Do while
+
+var age = 30;
+
+do{
+    alert("only age major than 25");
+    age -= 1;
+}while(age >= 25)
