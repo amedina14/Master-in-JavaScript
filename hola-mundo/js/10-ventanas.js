6d88dcd2cbdb82c5142cc2459fa3f32b88f9880c
Adrian Medina <medinalarry96@gmail.com>
2020-07-15 19:10:17 +0200

    alert, confirm, promt: insert data

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Alert
+alert("This is my alert!");
+alert("This is my Text");
+
+// Confirm
+var my_result = confirm("Are you sure want to continue?");
+console.log(my_result);
+
+// Insert Data
+var age = prompt("How old are you?", 18);
+console.log(age, typeof age);
