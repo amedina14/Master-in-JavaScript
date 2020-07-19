a28b47c5410125478f499948c159d6e5938d73b3
Adrian Medina <medinalarry96@gmail.com>
2020-07-19 20:44:42 +0200

    functions

---
+++ hola-mundo/js/19-funciones.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/**
+ * Funciones
+ * Insieme d'istruzioni reutilizzabili 
+ */
+
+ // Defino la function
+function calculadora() {
+    // Conjunto de instrucciones a ejecutar
+    console.log("Hola soy la calculadora!!");    
+    console.log("Si soy yo");    
+}
+
+// Invocar o llamar la funcion
+var resultado = calculadora();
+
+console.log(resultado);
