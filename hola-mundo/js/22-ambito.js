152f4aa2b0992b5b0d39319d1ab59ad30e552cd0
Adrian Medina <medinalarry96@gmail.com>
2020-07-21 00:28:28 +0200

    ambito de las variables

---
+++ hola-mundo/js/22-ambito.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function holaMundo(texto){
+    var hola_mundo = "Texto LOCAL dentro de funcion";
+
+    console.log(texto);
+    console.log(numero.toString(), typeof numero.toString());
+    console.log(hola_mundo);
+}
+
+var numero = 12;
+var texto = "Hola mundo soy gobal!";
+
+holaMundo(texto);
