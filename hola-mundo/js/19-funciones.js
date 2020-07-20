299ba3571e53da592acd481bc8ff3cba247105dc
Adrian Medina <medinalarry96@gmail.com>
2020-07-20 12:01:56 +0200

    functions within others

--- hola-mundo/js/19-funciones.js
+++ hola-mundo/js/19-funciones.js
@@ -5,33 +5,40 @@
  * Insieme d'istruzioni reutilizzabili 
  */
 
-// var numero1 =;
+function porConsola(numero1, numero2){
+    console.log("Hola soy la calculadora!!");
+    console.log("Suma:" + (numero1 + numero2));
+    console.log("Resta:" + (numero1 - numero2));
+    console.log("Mult:" + (numero1 * numero2));
+    console.log("Div:" + (numero1 / numero2));
+    console.log("****************************");
+}
+function porPantalla(numero1, numero2){
+    document.write("Hola soy la calculadora!!<br>");
+    document.write("Suma:" + (numero1 + numero2)+ "<br>");
+    document.write("Resta:" + (numero1 - numero2)+ "<br>");
+    document.write("Mult:" + (numero1 * numero2)+ "<br>");
+    document.write("Div:" + (numero1 / numero2)+ "<br>");
+    document.write("****************************<br>");
+}
 
 // Defino la function
 function calculadora(numero1, numero2, mostrar = false) {
     // Conjunto de instrucciones a ejecutar
 
     if (mostrar == false) {
-        console.log("Hola soy la calculadora!!");
-        console.log("Suma:" + (numero1 + numero2));
-        console.log("Resta:" + (numero1 - numero2));
-        console.log("Mult:" + (numero1 * numero2));
-        console.log("Div:" + (numero1 / numero2));
-        console.log("****************************");
+        porConsola(numero1,numero2);
     } else {
-        document.write("Hola soy la calculadora!!<br>");
-        document.write("Suma:" + (numero1 + numero2)+ "<br>");
-        document.write("Resta:" + (numero1 - numero2)+ "<br>");
-        document.write("Mult:" + (numero1 * numero2)+ "<br>");
-        document.write("Div:" + (numero1 / numero2)+ "<br>");
-        document.write("****************************");
+        porPantalla(numero1,numero2);
     }
-    return "retornooo";
+    return "true";
 }
 
 // Invocar o llamar la funcion
-calculadora(1,8,false);
+calculadora(1,8);
 calculadora(2,8,true);
+calculadora(2,8,true);
+calculadora(10,8);
 
 /**
  * var resultado = calculadora();
