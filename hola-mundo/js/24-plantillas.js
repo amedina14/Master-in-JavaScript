ea95c514057e771c2d53b926c12584a70d01d167
Adrian Medina <medinalarry96@gmail.com>
2020-07-21 14:18:05 +0200

    plantillas, ejemplo form

--- hola-mundo/js/24-plantillas.js
+++ hola-mundo/js/24-plantillas.js
@@ -2,14 +2,18 @@
 
 // Plantillas de texto
 
-var nombre = prompt("Inserta nombre: ");
-var apellidos = prompt("Inserta apellidos: ");
+var id = prompt("ID sitio: ");
+var nombre = prompt("nombre sitio: ");
+var causa = prompt("Causa: ");
+var solucion = prompt("Solucion: ");
 
 //var texto = ""+nombre+""+apellidos;
 var texto = `
     <h1>Plantilla</h1>
-    <h3>Mi nombre es: ${nombre}</h3>
-    <h3>Mi apellidos son: ${apellidos}</h3>
+    <h3>ID sitio: ${id}</h3>
+    <h3>Nombre sitio: ${nombre}</h3>
+    <h3>Causa: ${causa}</h3>
+    <h3>Solucion: ${solucion}</h3>
 `;
 
 document.write(texto);
