f4363e748b95aa7a8253714b2384fe2f053ed1d8
Adrian Medina <medinalarry96@gmail.com>
2020-07-21 20:05:34 +0200

    Array avanzados

--- hola-mundo/js/25-arrays.js
+++ hola-mundo/js/25-arrays.js
@@ -3,9 +3,28 @@
 // Arrays, arreglos, vectores, matrices.
 
 var nombre = "Adrian";
-var nombres = ["nola","dfbvhd","qee","qwty","varey",52,true];
+var nombres = ["Riko", "Honey", "Miel", "Qiubyk", "Varey", 52, true];
 
-var languages = new Array("PHP","JS","Go","Java");
+var languages = new Array("PHP", "JS", "Go", "Java","C","C#","Python");
 
-console.log(nombres[2]);
-console.log(languages);
+console.log("nombre 1:", nombres[1]);
+console.log("chars nombre 1:", nombres[1].length);
+console.log("languages: ", languages.length);
+console.log("chars nombre:", nombre.length);
+
+/*
+var elemento = parseInt(prompt("elemento: ",""));
+if (elemento >= nombres.length){
+    alert("no hay.. ("+nombres.length+")");
+}else{
+    console.log("El usuario seleccionado es:",nombres[elemento]);
+    alert("El usuario seleccionado es: "+nombres[elemento]);
+}
+*/
+
+document.write("<h1>Lenguajes de programacion del 2018: </h1>");
+document.write("<ul>");
+for (var i = 0; i < languages.length; i++) {
+    document.write("<li> "+languages[i]+"</li>");
+}
+document.write("</ul>");
