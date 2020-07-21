de401d540ddda251937db0075c57e724787e2d64
Adrian Medina <medinalarry96@gmail.com>
2020-07-21 16:33:14 +0200

    Array basicos

---
+++ hola-mundo/js/25-arrays.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Arrays, arreglos, vectores, matrices.
+
+var nombre = "Adrian";
+var nombres = ["nola","dfbvhd","qee","qwty","varey",52,true];
+
+var languages = new Array("PHP","JS","Go","Java");
+
+console.log(nombres[2]);
+console.log(languages);
