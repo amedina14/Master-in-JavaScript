b33638a439488ec5fbb51d4c068c99a4a7f3bb41
Adrian Medina <medinalarry96@gmail.com>
2020-07-21 13:29:17 +0200

    funciones de reemplazo

--- hola-mundo/js/23-funciones-texto.js
+++ hola-mundo/js/23-funciones-texto.js
@@ -4,23 +4,32 @@
  * Transformacion cadenas de texto
  */
 
-// Metodos de busqueda
-
 var numero = "40";
 //var texto1 = "Curso Master JS";
 var texto1 = "Bienvenido al curso de JavaScript de Victor Robles";
 var texto2 = "Es muy buen curso";
-var busqueda = texto1.indexOf("curso");
-busqueda = texto1.lastIndexOf("de");
-busqueda = texto1.search("JavaScript");
-busqueda = texto1.match(/de/gi);
-busqueda = texto1.substr(14, 5);
-busqueda = texto1.charAt(44);
-busqueda = texto1.startsWith("Bienvenido");
-busqueda = texto1.endsWith("Robles");
-busqueda = texto1.includes("JavaScript");
+var busqueda = texto1.indexOf("curso"); //la primera posicion en que sale la palabra
+
+// Metodos de busqueda
+
+busqueda = texto1.lastIndexOf("de"); //la ultima posicion en que sale la palabra
+busqueda = texto1.search("JavaScript"); //dice el punto de donde inicia la palabra
+busqueda = texto1.match(/de/gi); //busca las veces que sale la palabra con RegEx
+busqueda = texto1.substr(14, 5); //busca desde un punto el numero de caracteres indicados
+busqueda = texto1.charAt(44); //el caracter en tal posicion
+busqueda = texto1.startsWith("Bienvenido"); //si la cadena inicia con la palabra
+busqueda = texto1.endsWith("Robles"); //si la cadena termina con la palabra
+busqueda = texto1.includes("JavaScript"); //si la cadena contiene la palabra
+
+// Funciones de reemplazo
+
+busqueda = texto1.replace("JavaScript","JS"); //remplaza elemento por otro
+busqueda = texto1.slice(14, 22); //corta cadena desde la posicion
+busqueda = texto1.split(" "); //mete en array y se puede indicar separador
+busqueda = texto1.trim(); //Recorta los espacion por delante y detras. Ej. Util para user y password
 
 console.log(busqueda);
+
 
 /*
 // Metodos para procesar texto
