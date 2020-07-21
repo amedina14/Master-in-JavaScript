7cb57d4448d6922e1f7bb453d9e8454e2ccc649b
Adrian Medina <medinalarry96@gmail.com>
2020-07-21 23:59:23 +0200

    Matrix

---
+++ hola-mundo/js/26-arrays-multi.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* MATRICES */
+
+var categorias = ["action", "Horror", "Comedia"];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Gran Torino'];
+
+var cine = [categorias, peliculas];
+
+console.log(cine);
