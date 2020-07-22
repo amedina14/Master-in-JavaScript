ad7d79e69c66f3492e8a7fb0f61fabb8b6302611
Adrian Medina <medinalarry96@gmail.com>
2020-07-22 13:26:55 +0200

    Exercise Arrays: exercise statement

---
+++ hola-mundo/js/27-exercise-array.js
@@ -0,0 +1,13 @@
+'use stict'
+
+/**
+ * Programa que: 
+ * 1. pide 6 numeros por pantalla y los meta en un array.
+ * 2. tiene que mostrar todos sus elementos en la page y consola.
+ * 3. ordenarlo y mostrarlo
+ * 4. invertir su orden y mostrarlo.
+ * 5. mostrar cuantos elementos tiene el array.
+ * 6. Busqueda de un valor introducido por el user
+ *    que diga si lo encuentra y su indice.
+ *    (Usar funciones).
+ */
