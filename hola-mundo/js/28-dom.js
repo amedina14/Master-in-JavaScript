586bc31f0dfed6dc69aa2a4c7c1dd28a07da54b6
Adrian Medina <medinalarry96@gmail.com>
2020-07-24 11:57:01 +0200

    DOM: selecting, modding elements

--- hola-mundo/js/28-dom.js
+++ hola-mundo/js/28-dom.js
@@ -2,10 +2,12 @@
 
 // DOM
 
-function cambiaColor(color){
+function cambiaColor(color) {
     caja.style.background = color;
 }
 
+
+// Get elements by specific ID
 var caja = document.getElementById("micaja");
 var caja2 = document.querySelector("#micaja2");
 
@@ -28,3 +30,34 @@ caja2.style.width = "15%";
 caja2.textAlign = "center";
 caja2.style.margin = "10px";
 caja2.style.padding = "10px";
+
+// Get element by tags
+var todosLosDivs = document.getElementsByTagName('div');
+var contenidoEnTexto = todosLosDivs[2].textContent;
+console.log(contenidoEnTexto);
+
+var modBox3 = todosLosDivs[2].innerHTML = "box 3 modded";
+console.log(modBox3);
+
+var seccion = document.querySelector("#miseccion");
+var hr = document.createElement("hr");
+
+//todosLosDivs.forEach((valor, indice)=>{
+for (var valor in todosLosDivs) {
+    //console.log(todosLosDivs[valor]);
+    if (typeof todosLosDivs[valor].textContent == 'string' ) {
+        //crea elemento html <p>
+        var parrafo = document.createElement("p");
+        //agarra el valor que le llega en el bucle de los divs
+        var texto = document.createTextNode(todosLosDivs[valor].textContent);
+        //mete el texto dentro del parrafo
+        parrafo.appendChild(texto);
+        //mete el parrafo dentro de otro tag
+        seccion.append(parrafo);
+    }
+}
+//});
+seccion.append(hr);
+
+// Get element by class css
+var todasLasClases = document.getElementsByClassName('');
