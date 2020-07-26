8e5d59b0b91fc4ab6d91a0cbd82fd79ed574ae37
Adrian Medina <medinalarry96@gmail.com>
2020-07-26 18:53:31 +0200

    Timers

---
+++ hola-mundo/js/31-timers.js
@@ -0,0 +1,41 @@
+'use strict'
+
+window.addEventListener('load', () => {
+
+    /**
+     * Timers:
+     * 
+     * setTimeout: se ejecuta solo 1 ves.
+     */
+    function intervalo() {
+        var tiempo = setInterval(() => {
+            console.log("Set interval eject");
+            var header = document.querySelector("h1");
+
+            if (header.style.fontSize == "50px") {
+                header.style.fontSize = "35px";
+            } else {
+                header.style.fontSize = "50px";
+            }
+
+        }, 500);
+
+        return tiempo;
+    }
+
+    var tiempo = intervalo();
+    var stop = document.querySelector("#stop");
+
+    stop.addEventListener("click", () => {
+        alert("has parado la bomba");
+        clearInterval(tiempo);
+    });
+
+    var start = document.querySelector("#start");
+
+    start.addEventListener("click", () => {
+        alert("has iniciado la bomba");
+        intervalo();
+    });
+
+});
