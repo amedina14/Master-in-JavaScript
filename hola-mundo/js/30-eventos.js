e33938299f44c11fb75b0387a8bb0e8128de5f6a
Adrian Medina <medinalarry96@gmail.com>
2020-07-26 18:13:47 +0200

    addEventListener Load

--- hola-mundo/js/30-eventos.js
+++ hola-mundo/js/30-eventos.js
@@ -1,78 +1,89 @@
 'use strict'
 
 /**
- * Eventos del raton
+ * Carga el script cuando se cargan los elementos
+ * con evento LOAD
  */
 
-function cambiarColor() {
-    console.log("Me has dado clic");
-    var bg = boton.style.background;
-    if (bg == "lime") {
-        boton.style.background = "skyblue";
-    } else {
-        boton.style.border = "none";
-        //  boton.style.textshadow = "1px 1px 15px grey";
-        boton.style.background = "lime";
+window.addEventListener('load', () => {
+
+    /**
+     * Eventos del raton
+     */
+
+    function cambiarColor() {
+        console.log("Me has dado clic");
+        var bg = boton.style.background;
+        if (bg == "lime") {
+            boton.style.background = "skyblue";
+        } else {
+            boton.style.border = "none";
+            //  boton.style.textshadow = "1px 1px 15px grey";
+            boton.style.background = "lime";
+        }
+
+        boton.style.padding = "10px";
+        boton.style.border = "1px solid #ccc";
+
+        return true;
     }
 
-    boton.style.padding = "10px";
-    boton.style.border = "1px solid #ccc";
+    /**
+     * Conecta el script al html, 
+     * es un escuchador de eventos separado del html.
+     */
 
-    return true;
-}
+    var boton = document.querySelector("#boton");
 
-/**
- * Conecta el script al html, 
- * es un escuchador de eventos separado del html.
- */
+    // click e dblclick
 
-var boton = document.querySelector("#boton");
+    boton.addEventListener('click', () => {
+        cambiarColor();
+    });
 
-// click e dblclick
+    // Mouse over
 
-boton.addEventListener('click', () => {
-    cambiarColor();
-});
+    boton.addEventListener('mouseover', () => {
+        boton.style.background = "pink";
+    });
 
-// Mouse over
+    // Mouse out
 
-boton.addEventListener('mouseover', () => {
-    boton.style.background = "pink";
-});
+    boton.addEventListener('mouseout', function () {
+        boton.style.background = "royalblue";
+    });
 
-// Mouse out
+    // Focus
 
-boton.addEventListener('mouseout', function(){
-    boton.style.background = "royalblue";
-});
+    var campo_nombre = document.querySelector("#campo_nombre");
+    campo_nombre.addEventListener('focus', () => {
+        console.log("Focus dentro del campo nombre");
+    });
 
-// Focus
+    // Blur
 
-var campo_nombre = document.querySelector("#campo_nombre");
-campo_nombre.addEventListener('focus', ()=>{
-    console.log("Focus dentro del campo nombre");
-});
+    campo_nombre.addEventListener('blur', () => {
+        console.log("Blur del campo nombre");
+    });
 
-// Blur
+    // KeyDown
 
-campo_nombre.addEventListener('blur', ()=>{
-    console.log("Blur del campo nombre");
-});
+    campo_nombre.addEventListener('keydown', (evento) => {
+        console.log("Pulsando este tecla:", String.fromCharCode(evento.keyCode));
+    });
 
-// KeyDown
+    // KeyPress
 
-campo_nombre.addEventListener('keydown', (evento)=>{
-    console.log("Pulsando este tecla:", String.fromCharCode(evento.keyCode));
-});
+    campo_nombre.addEventListener('keypress', (evento) => {
+        console.log("Tecla Pulsada:", String.fromCharCode(evento.keyCode));
+    });
 
-// KeyPress
+    // KeyUp
 
-campo_nombre.addEventListener('keypress', (evento)=>{
-    console.log("Tecla Pulsada:", String.fromCharCode(evento.keyCode));
-});
+    campo_nombre.addEventListener('keyup', (evento) => {
+        console.log("Tecla soltada:", String.fromCharCode(evento.keyCode));
+    });
 
-// KeyUp
+}); //end Load
 
-campo_nombre.addEventListener('keyup', (evento)=>{
-    console.log("Tecla soltada:", String.fromCharCode(evento.keyCode));
-});
+
