32fa61923c074698b43e38981ae51a442ec19b84
Adrian Medina <medinalarry96@gmail.com>
2020-08-04 15:08:46 +0200

    Promesas y Fetch

--- hola-mundo/js/36-fetch.js
+++ hola-mundo/js/36-fetch.js
@@ -3,22 +3,52 @@
 //Fetch (ajax) y peticiones a servicios / aps rest
 
 var div_usuarios = document.querySelector("#usuarios");
-var usuarios = [];
+var div_janet = document.querySelector("#janet");
+
+
 window.addEventListener('load', function () {
-    //alert("hola");
     //https://jsonplaceholder.typicode.com/users
-    fetch('https://reqres.in/api/users')
+    getUsuarios()
         .then(datos => datos.json())
         .then(users => {
-            usuarios = users.data;
             console.log(usuarios);
+            listadoUsuarios(users.data);
 
-            usuarios.map((user, i) => {
-                let nombre = document.createElement('h3');
-                nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
-                div_usuarios.appendChild(nombre);
-
-                document.querySelector(".loading").style.display = "none";
-            });
+            return getJanet();
+        })
+        .then(data => data.json())
+        .then(user => {
+            mostrarJanet(user.data);
         });
+
+    function getJanet() {
+        return fetch('https://reqres.in/api/users/2');
+    }
+
+    function getUsuarios() {
+        return fetch('https://reqres.in/api/users');
+    }
+
+    function listadoUsuarios(usuarios) {
+        usuarios.map((user, i) => {
+            let nombre = document.createElement('h3');
+            nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
+            div_usuarios.appendChild(nombre);
+
+            document.querySelector(".loading").style.display = "none";
+        });
+    }
+
+    function mostrarJanet(user) {
+        let nombre = document.createElement('h4');
+        let avatar = document.createElement('img');
+        nombre.innerHTML = user.first_name + " " + user.last_name;
+        avatar.src = user.avatar;
+        avatar.width = '100';
+        div_janet.appendChild(nombre);
+        div_janet.appendChild(avatar);
+
+        document.querySelector("#janet .loading").style.display = "none";
+    }
+
 });
