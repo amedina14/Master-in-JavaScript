3b3a58daa03a21d868deb0763a535f9e1f7c73e5
Adrian Medina <medinalarry96@gmail.com>
2020-08-05 23:46:34 +0200

    JQ: Selectores de Clases

--- aprendiendo-Jquery/js/01-selectoresJQ.js
+++ aprendiendo-Jquery/js/01-selectoresJQ.js
@@ -14,4 +14,22 @@ $(document).ready(function () {
 
     var verde = $("#verde").css("background", "green")
         .css("color", "yellow");
-})
+
+    // Selector de clase
+    var mi_clase = $('.zebra').css("padding","5px");
+
+    $(".sin_borde").click(function(){
+        console.log("has dado click");
+        /* Agrega clase solo a ese elemento 
+        con click. Para que funcione debe haber
+        un estilo predefinido.
+        */
+       $(this).addClass("zebra");
+    });
+
+    /* con eq tengo metodos de JQ ya adjuntos y
+    puedo hacer mas cosas.
+
+    console.log(mi_clase.eq(1));    
+    */
+});
