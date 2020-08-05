9f5c0ab4c83c1830befc3e63536e4a2cd5aa3543
Adrian Medina <medinalarry96@gmail.com>
2020-08-05 12:09:06 +0200

    Comentarios del codigo

--- hola-mundo/js/32-ejercicio-dom.js
+++ hola-mundo/js/32-ejercicio-dom.js
@@ -7,17 +7,21 @@
 window.addEventListener('load', ()=>{
     console.log("DOM cargado");
 
+    /*Captura el form y el dashed form */
     var formulario = document.querySelector("#formulario");
     var box_dashed = document.querySelector(".dashed");
     box_dashed.style.display = "none";
 
+    /*Captura el envio del form */
     formulario.addEventListener('submit', ()=>{
         console.log("Evento submit capturado");
 
+        /**Captura elementos del form */
         var nombre = document.querySelector("#nombre").value;
         var apellidos = document.querySelector("#apellidos").value;
         var edad = parseInt(document.querySelector("#edad").value);
 
+        /**Valida elementos del form */
         if(nombre.trim() == null || nombre.trim().length == 0){
             alert("Nombre no valido");
             var msg_error_name = document.querySelector("#error_nombre");
@@ -41,12 +45,17 @@ window.addEventListener('load', ()=>{
             document.querySelector("#error_edad").style.display = "none";
         }
 
+        /**Activa dashed para mostrar elementos */
         box_dashed.style.display = "block";
 
+        /**Variables JS de tags del dashed */
         var p_nombre = document.querySelector("#p_nombre span");
         var p_apellidos = document.querySelector("#p_apellidos span");
         var p_edad = document.querySelector("#p_edad span");
 
+        /**mete elemento form 
+         * dentro de variable js,
+         * dentro del html */
         p_nombre.innerHTML = nombre;
         p_apellidos.innerHTML = apellidos;
         p_edad.innerHTML = edad;
