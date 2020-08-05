785de144124861481f9cafc3c70f14ad00105ea7
Adrian Medina <medinalarry96@gmail.com>
2020-08-05 12:53:25 +0200

    Fx Math en JS: random parseInt then print in layout

--- hola-mundo/js/38-fechas.js
+++ hola-mundo/js/38-fechas.js
@@ -6,13 +6,17 @@ var mes = fecha.getUTCMonth();
 var day = fecha.getUTCDate();
 var hora = fecha.getHours();
 
-var textoHora = `
-    El año es: ${year}
-    El mes es: ${mes}
-    El dìa es: ${day}
-    La hora es: ${hora}
-`;
-console.log(textoHora);
 /**Numero casual de 1 a 10,
  * aproximando los decimales */
-console.log(Math.ceil(Math.random()*10));
+var casual = parseInt(Math.ceil(Math.random() * 10));
+
+var textoHora = `
+El año es: ${year}
+El mes es: ${mes}
+El dìa es: ${day}
+La hora es: ${hora}
+Numero casual: ${casual}
+`;
+
+console.log(textoHora);
+console.log(casual);
