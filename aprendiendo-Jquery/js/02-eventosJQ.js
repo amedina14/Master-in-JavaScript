41f935f1a70094472b2c0121559c895a671899dd
Adrian Medina <medinalarry96@gmail.com>
2020-08-07 11:27:34 +0200

    Mouse move: creando cursor y mas ejemplos

--- aprendiendo-Jquery/js/02-eventosJQ.js
+++ aprendiendo-Jquery/js/02-eventosJQ.js
@@ -43,15 +43,34 @@ $(document).ready(function () {
 
     // Focus y blur
     var nombre = $('#nombre');
-    nombre.focus(function(){
-        $(this).css('border','2px solid green');
+    var datos = $('#datos');
+    nombre.focus(function () {
+        $(this).css('border', '2px solid green');
     });
-    nombre.blur(function(){
-        $(this).css('border','1px solid #ccc');
+    nombre.blur(function () {
+        $(this).css('border', '1px solid #ccc');
         //$('#datos').css('display','block');
         var valor = $(this).val(); // agarra el valor del form
-        $('#datos').text(valor).show(); // text: incrusta valor dentro de otra etiqueta.
+        datos.text(valor).show(); // text: incrusta valor dentro de otra etiqueta.
         // show, contrario de hide, muestra elemento oculto.
     });
 
+    // Mousedown y mouseup
+    datos.mousedown(function () {
+        $(this).css("border-color", "orange");
+    });
+    datos.mouseup(function () {
+        $(this).css("border-color", "navy");
+    });
+
+    // mouse move
+    var cursor = $('.cursor');
+    //var latitud = $(document).event.clientY;
+    //var longitud = $(document).event.clientX;
+    $(document).mousemove(function () {
+        console.log('Coordinates: ' + event.clientY + ', ' + event.clientX);
+        $('body').css('cursor','none');
+        cursor.css('left', event.clientX)
+            .css('top', event.clientY);
+    });
 });
