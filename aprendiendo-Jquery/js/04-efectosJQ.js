ed7dd94921cdd6681975f126590f028bc4a11e7e
Adrian Medina <medinalarry96@gmail.com>
2020-08-08 13:35:42 +0200

    Efectos en JQuery

---
+++ aprendiendo-Jquery/js/04-efectosJQ.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    var caja = $('#caja');
+    var mostrar = $('#mostrar');
+    var ocultar = $('#ocultar');
+    var toggle = $('#toggle');
+
+    mostrar.hide();
+    
+    mostrar.click(function(){
+        $(this).hide();
+        ocultar.show();
+        caja.fadeTo('slow',0.8);
+    });
+
+    /**
+     * fadeIn('slow')
+     * fadeOut('fast')
+     * show('fast')
+     * hide('low')
+     * slideUp() //ocultar
+     * slideDown() //mostrar
+     */
+
+    ocultar.click(function(){
+        $(this).hide();
+        mostrar.show();
+        caja.fadeTo('slow',0.2);
+    });
+
+    // Togle
+    toggle.click(function(){
+        caja.slideToggle('fast');
+        /**
+         * toggle
+         * fadeToggle
+         * slideToggle
+         */
+        /*
+        if(caja.hide()){
+            caja.show();
+        } else {
+            caja.hide();
+        }
+        */
+    });
+
+});
