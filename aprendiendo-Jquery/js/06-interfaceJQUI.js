df3caeac54da6b7d018c6b1351bd7f52645115b5
Adrian Medina <medinalarry96@gmail.com>
2020-08-27 11:54:05 +0200

    calenderio: datepicker

--- aprendiendo-Jquery/js/06-interfaceJQUI.js
+++ aprendiendo-Jquery/js/06-interfaceJQUI.js
@@ -36,4 +36,7 @@ $(document).ready(function(){
     $("#lanzar-popup").click(function(){
         $("#popup").dialog();
     });
+
+    // Calendario / Datepicker
+    $("#calendario").datepicker();
 });
