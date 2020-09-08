a3638f8b4dbf3c66a8be4e46f42da942ef0bd645
Adrian Medina <medinalarry96@gmail.com>
2020-09-08 10:44:45 +0200

    Let vs Var en TS

--- poo-y-typescript/Js/03-tipos-de-datos.js
+++ poo-y-typescript/Js/03-tipos-de-datos.js
@@ -11,4 +11,13 @@ var anywhere = "hola";
 // Arrays
 var languages = ['PHP', 'Js', 'CSS'];
 var year = ["DOCE", 13, 14];
+// Let vs Var
+var numero1 = 10;
+var numero2 = 12;
+if (numero1 == 10) {
+    var numero1_1 = 44;
+    var numero2 = 55;
+    console.log(numero1_1, numero2);
+}
+console.log(numero1, numero2);
 console.log(cadena, numero, verdadero_falso, anywhere, languages);
--- poo-y-typescript/Js/03-tipos-de-datos.ts
+++ poo-y-typescript/Js/03-tipos-de-datos.ts
@@ -21,4 +21,17 @@ var languages: Array<string> = ['PHP', 'Js', 'CSS'];
 
 let year: any[] = ["DOCE", 13, 14];
 
+// Let vs Var
+var numero1 = 10;
+var numero2 = 12;
+
+if(numero1 == 10){
+    let numero1 = 44;
+    var numero2 = 55;
+
+    console.log(numero1, numero2);
+}
+
+console.log(numero1, numero2);
+
 console.log(cadena, numero, verdadero_falso, anywhere, languages);
