a8f7d36d8d32009b2a98375fd86386fdf794e320
Adrian Medina <medinalarry96@gmail.com>
2020-09-08 11:10:28 +0200

    Funciones y tipado fuerte

---
+++ poo-y-typescript/Js/04-funciones.js
@@ -0,0 +1,6 @@
+// entra number sale string
+function getNumero(numero) {
+    if (numero === void 0) { numero = 12; }
+    return "El numero es: " + numero;
+}
+console.log(getNumero(55));
---
+++ poo-y-typescript/Js/04-funciones.ts
@@ -0,0 +1,7 @@
+// entra number sale string
+
+function getNumero(numero:number = 12):string{
+    return "El numero es: "+numero;
+}
+
+console.log(getNumero(55));
