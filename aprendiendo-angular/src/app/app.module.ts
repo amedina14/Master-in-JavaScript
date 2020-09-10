5a99acdceafccbc7b90ddef5433f0f9ac7d0b601
Adrian Medina <medinalarry96@gmail.com>
2020-09-10 17:42:28 +0200

    Generate components by cli

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillaComponent } from './zapatillas/zapatillas.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ZapatillaComponent,
-    VideojuegoComponent
+    VideojuegoComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
