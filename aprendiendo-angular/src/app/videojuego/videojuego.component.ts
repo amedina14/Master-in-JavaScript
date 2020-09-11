1c84ff7cb4fd758588738ffe9338c0ea1a5caed0
Adrian Medina <medinalarry96@gmail.com>
2020-09-11 10:49:13 +0200

    Hooks / Eventos ciclo de vida de componentes

--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'videojuego',
     templateUrl: './videojuego.component.html' 
 })
 
-export class VideojuegoComponent {
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy{
     public titulo: string;
     public listado: string;
 
@@ -13,5 +13,34 @@ export class VideojuegoComponent {
         this.titulo = "Componente de videojuegos";
         this.listado = "Listado de los juegos mas populares";
         console.log("Se ha cargado el componente: videojuego.component.ts");
+    }
+
+    // Se exe al cargar el componente
+    ngOnInit(){
+        console.log("OnInit executed!");
+    }
+
+    // Se exe a cada cambio del codigo.
+    ngDoCheck(){
+        console.log("DoCheck executed");
+    }
+
+    // cambia nombre con boton + evento en la vista.
+    /**
+     * Util para una variable que estè siempre
+     * actualizada, recoger unos datos cada vez que 
+     * haya un cambio en la app.
+     */
+    cambiarTitulo(){
+        this.titulo = "Nuevo titulo del componente";
+    }
+
+    /**
+     * Permite ejecucion de algo, antes que
+     * se BORRE o OCULTE la instancia de un componente
+     * en la vista.
+     */
+    ngOnDestroy(){
+        console.log("OnDestroy executed!");
     }
 }
