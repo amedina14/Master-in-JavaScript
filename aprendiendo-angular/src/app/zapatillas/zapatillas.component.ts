3406e4e70b76c147c10e5a8f498b68f6a76df7c2
Adrian Medina <medinalarry96@gmail.com>
2020-09-12 12:31:10 +0200

    Directiva estructural *ngFor: Lista de marcas unicas

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -9,20 +9,44 @@ import { Zapatilla } from '../models/zapatilla';
 export class ZapatillaComponent implements OnInit {
 
     public titulo: string;
-    public zapatillas: Array<Zapatilla>;
+    public zapatillas: Array<Zapatilla>; // Decl. arr. zap.
+    public marcas: String[]; // Declaracion array string
 
     constructor(){
         this.titulo = "Componente Zapatillas";
 
         this.zapatillas = [
-            new Zapatilla("Chancletality", 100, "Meme", "red", true),
+            new Zapatilla("Gazzelle", 40, "Adidas", "blue", false),
+            new Zapatilla("Chancletality", 100, "Nike", "red", true),
             new Zapatilla("Gazzelle", 90, "Adidas", "green", true),
             new Zapatilla("Boots Cowboy", 39, "Mr Jones", "brown", true),
-        ]
+        ];
+
+        // Se inicializa el array vacio
+        this.marcas = new Array();
     }
 
     // Muestro por consola apenas inicia
     ngOnInit(){
         console.log(this.zapatillas);
+        this.getMarcas(); // invoco metd.
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) => {
+
+            /**
+             * indexOf: busqueda dentro array.
+             * x < 0: no existe.
+             * Del array de marcas busca si la marca
+             * no existe y la agrega en el array.
+             *  */ 
+            if(this.marcas.indexOf(zapatilla.marca) < 0){ //push marca solo si no existe ya
+                this.marcas.push(zapatilla.marca);
+            }
+
+            //console.log(index);
+        });
+        console.log(this.marcas); // array en consola
     }
 }
