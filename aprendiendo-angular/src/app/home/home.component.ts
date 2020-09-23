39bc5bdbe88d2fecf4e3a2d4e214195ef07c8fae
Adrian Medina <medinalarry96@gmail.com>
2020-09-23 12:55:42 +0200

    Direttiva ngTemplate per impostare l'else all'if

--- aprendiendo-angular/src/app/home/home.component.ts
+++ aprendiendo-angular/src/app/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public identificado: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setIdentificacion(){
+    this.identificado = true;
+  }
+
+  unsetIdentificacion(){
+    this.identificado = false;
   }
 
 }
