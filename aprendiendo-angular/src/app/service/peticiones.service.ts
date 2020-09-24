e944d498267bcd2bf8848f1964d7b93f2bb66d0d
Adrian Medina <medinalarry96@gmail.com>
2020-09-24 14:02:15 +0200

    Solving error rxjs/Observable | npm install --save rxjs-compat

--- aprendiendo-angular/src/app/service/peticiones.service.ts
+++ aprendiendo-angular/src/app/service/peticiones.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 // Con esto se recoje info de apirest
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PeticionesService {
