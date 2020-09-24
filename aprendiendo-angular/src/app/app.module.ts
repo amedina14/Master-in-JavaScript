212a40becaf2c6dc3c1f29b5fc6283dd7efebd7e
Adrian Medina <medinalarry96@gmail.com>
2020-09-24 13:40:25 +0200

    Custom Pipes | pipes personalizzati

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ZapatillaComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { ExternoComponent } from './externo/externo.component';
+import { CalculadoraPipe } from './pipes/calculadora.pipe';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ExternoComponent } from './externo/externo.component';
     CursosComponent,
     HomeComponent,
     ExternoComponent,
+    CalculadoraPipe
   ],
   imports: [
     BrowserModule,
