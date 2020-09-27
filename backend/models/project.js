40d854f75d2b96a83cfcb21fedd66e2b73597689
Adrian Medina <medinalarry96@gmail.com>
2020-09-27 19:48:51 +0200

    Creare models

---
+++ backend/models/project.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Objeto molde para crear nuevos documentos de tipo "proyecto"
+const ProjectSchema = Schema({
+    name: String,
+    description: String,
+    category: String,
+    year: Number,
+    langs: [String]
+});
+
+/**
+ * 1. Para poder exportar
+ * 2. Agarra este schema y lo usa como modelo
+ * 3. param1 nombre entidad
+ *    param2 nombre schema
+ * 4. mongoose parsea minuscola y en plural 
+ * la entidad. Save docments in collections
+ * (registros en tablas).
+ */
+module.exports = mongoose.model('Project', ProjectSchema);
