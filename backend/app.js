3158a33df66bfd81577fad563b5ca68148079fcb
Adrian Medina <medinalarry96@gmail.com>
2020-10-02 19:46:36 +0200

    Config Headers HTTP and CORS access middleware in nodejs

--- backend/app.js
+++ backend/app.js
@@ -14,12 +14,22 @@ const projectRoutes = require('./routes/projectRoutes');
  * 1. Config necesaria para bodyParser.
  * 2. Convierte lo que llegue a json
  * 3. Con app.use() se crean los middleware
+ * 4. Config midd per i cors su nodejs (Frontend-backend)
  */
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // CORS
-
+/**
+ * Permite acceso de un dominio/origen a otro. Compatibilidades con frontend.
+ */
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin:','*');
+    res.header('Access-Control-Allow-Headers','Authorization,X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,DELETE');
+    res.header('Allow','GET,POST,OPTIONS,PUT,DELETE');
+    next();
+});
 
 // Rutas
 app.use('/api', projectRoutes);
