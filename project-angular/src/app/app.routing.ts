dbfa5df48e3f382c9d0504042e6222efc7fa7075
Adrian Medina <medinalarry96@gmail.com>
2020-10-03 13:20:41 +0200

    Creando e definendo Routes dell'app nel modulo routing

---
+++ project-angular/src/app/app.routing.ts
@@ -0,0 +1,40 @@
+// importacion de modulos de angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//import { from } from 'rxjs';
+
+// Importacion de componentes creados
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from './components/error/error.component';
+
+// Creacion y definicion de rutas.
+const appRoutes: Routes = [
+    {
+        path: '', component: AboutComponent
+    },
+    {
+        path: 'about', component: AboutComponent
+    },
+    {
+        path: 'projects', component: ProjectsComponent
+    },
+    {
+        path: 'create', component: CreateComponent
+    },
+    {
+        path: 'contact', component: ContactComponent
+    },
+    {
+        path: '**', component: ErrorComponent
+    }
+];
+
+// exportar servicios y modulo/fichero de configuracion de rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
+/**
+ * Para asociar una ruta a un componente y hacerla funcionar, debemos usar el router-outlet
+*/
