e008ad0713d6ba15244531d6df2d9eab88b16e97
Adrian Medina <medinalarry96@gmail.com>
2020-10-06 21:14:47 +0200

    Responsive Navbar for smartphone

--- project-angular/src/app/app.component.ts
+++ project-angular/src/app/app.component.ts
@@ -7,19 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-angular';
+  //public windowWidth: any = window.innerWidth;
+  //public navPhone: any = document.getElementById('nav-phone');
+
+  ngOnInit() {
+    const navPhone = document.getElementById('nav-phone');
+    const headerOnInit = document.getElementById('header');
+    const contentOnInit = document.getElementById('content');
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth <= 623) {
+      navPhone.style.display = 'block';
+    }
+    if(windowWidth > 623){
+      navPhone.style.display = 'block';
+      //headerOnInit.style.minHeight = ;
+    }
+  }
 
   myFunction() {
-    const navPhone = document.getElementById('nav-phone');
+    const navPhone2 = document.getElementById('nav-phone');
     const header = document.getElementById('header');
     const content = document.getElementById('content');
-    if(navPhone.style.display === "block"){
-      header.style.minHeight = '90px';
-      content.style.paddingTop = '100px';
-      navPhone.style.display = "none";
-    } else {
-      navPhone.style.display = "block";
-      content.style.paddingTop = '270px';
-      header.style.minHeight = '260px';
+    //const links = document.getElementsByClassName('linkPhone');
+    const windowWidth2 = window.innerWidth;
+    if (windowWidth2 <= 623) {
+      if (navPhone2.style.display === "block") {
+        header.style.minHeight = '90px';
+        content.style.paddingTop = '100px';
+        navPhone2.style.display = "none";
+      } else {
+        navPhone2.style.display = "block";
+        content.style.paddingTop = '270px';
+        header.style.minHeight = '260px';
+      }
     }
   }
 }
