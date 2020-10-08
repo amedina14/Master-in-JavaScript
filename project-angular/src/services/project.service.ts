42c16a697a15a88129ba9d584c134cfb43812aa1
Adrian Medina <medinalarry96@gmail.com>
2020-10-08 22:57:08 +0200

    Creare la funzione del servizio che converte i dati del progetto a json e li restituisce

--- project-angular/src/services/project.service.ts
+++ project-angular/src/services/project.service.ts
@@ -17,4 +17,24 @@ export class ProjectService{
     testService(){
         return 'Metodo de prueba, servicio funcionando';
     }
+
+    /**
+     * Variables de la funcion: parametros y headers.
+     * 
+     * Para guarda un objeto json en la base de datos (mongoDB) debemos retornar las siguientes instrucciones:
+     * return: resultado de la funcion.
+     * this._http: variable que almacena la libreria HttpClient.
+     * post(): metodo de la clase/libreria anterior.
+     * this.url: api del backend como parametro de post.
+     * save-project: ruta concatenada a la url/api.
+     * params: variable de parametros del objeto json.
+     * {headers: headers}: json que contiene la variable de las cabeceras del objeto.
+     * La funcion devuelve un OBSERVABLE de cualquier tipo.
+    */
+    saveProject(project: Project): Observable<any>{
+        let params = JSON.stringify(project);
+        let headers = new HttpHeaders().set('Content-Type','application/json');
+
+        return this._http.post(this.url+'save-project', params, {headers: headers});
+    }
 }
