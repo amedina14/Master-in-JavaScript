3f319a0444b9cb89cf321c118ea335b8b636f1f9
Adrian Medina <medinalarry96@gmail.com>
2020-10-09 14:05:47 +0200

    Consumare il servizio uploadService sul componente create project

--- project-angular/src/app/components/create/create.component.ts
+++ project-angular/src/app/components/create/create.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/models/project';
+import { UploadService } from 'src/services/upload.service';
 import { ProjectService } from '../../../services/project.service';
+import { Global } from '../../../services/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService] //Aggiunge servizio al decoratore
+  providers: [ProjectService, UploadService] //Aggiunge servizio al decoratore
 })
 export class CreateComponent implements OnInit {
 
   public title: String;
   public project: Project;
   public status: String;
+  public filesToUpload: Array<File>;
 
   /**
    * Dentro de los parametros del constructor se crean las propiedades del servicio
    * @param _projectService Propiedades del servicio
    */
   constructor(
-    private _projectService: ProjectService
-  ) { 
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
+  ) {
     this.title = "Aggiungere nuovo progetto";
-    this.project = new Project('','','','',2020,'','');
+    this.project = new Project('', '', '', '', 2020, '', '');
   }
 
   ngOnInit(): void {
@@ -36,12 +40,23 @@ export class CreateComponent implements OnInit {
    * 
    * @param form Datos del formulrio.
    */
-  onSubmit(form){
+  onSubmit(form) {
+
+    // Guardar los datos
     this._projectService.saveProject(this.project).subscribe(
       response => {
-        if(response.project){
-          this.status = 'success';
-          form.reset();
+        if (response.project) {
+
+          // Subir la imagen
+          this._uploadService.makeFileRequest(Global.url + 'uploadImage/' + response.project._id, [], this.filesToUpload, 'image')
+            .then((result: any) => {
+
+              console.log(result);
+
+              this.status = 'success';
+              form.reset();
+            });
+
         } else {
           this.status = 'failed';
         }
@@ -50,6 +65,21 @@ export class CreateComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  /**
+   * Captura todos los datos relativos al file.
+   * 
+   * @param fileInput file cargado
+   */
+  fileChangeEvent(fileInput: any) {
+    /**
+     * console.log(fileInput); sirve para encontrar el file en
+     * propiedad target.
+     * Convierto obligatoriamente los datos a un array.
+     * Ya tengo en la propiedad los datos que quiero subir.
+     */
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
