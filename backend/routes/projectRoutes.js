b123719da9ca208b59363b2a7371ef96b8f552f4
Adrian Medina <medinalarry96@gmail.com>
2020-10-11 01:54:58 +0200

    Aggiungere indirizzo /get-image con il suo parametro

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -20,8 +20,9 @@ const multipart = require('connect-multiparty');
 
 /**
  * Defino Middleware que llama a connect-multiparty y guarda 
- * imagenes en directory. Este middleware va usado antes de llamar
- * al metodo. Se coloca como segundo parametro en la definicion de
+ * imagenes en directory. 
+ * Este middleware va usado antes de llamar al metodo. 
+ * Se coloca como segundo parametro en la definicion de
  * la url para que se ejecute antes del metodo que hace la accion.
 */
 const multipartMiddleware = multipart({
@@ -36,6 +37,8 @@ router.get('/projects', projectController.getProjects);
 // Para actualizar recursos en un API se usa PUT
 router.put('/update/:id', projectController.updateProject);
 router.delete('/delete/:id', projectController.deleteProject);
+// invoco el middleware connect-multiparty
 router.post('/uploadImage/:id', multipartMiddleware, projectController.uploadImage);
+router.get('/get-image/:image', projectController.getImageFile);
 
 module.exports = router;
