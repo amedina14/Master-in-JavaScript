fb130e5f57958c3e6ebe7263452395f8a37b0a00
Adrian Medina <medinalarry96@gmail.com>
2020-10-14 18:29:19 +0200

    Metodo delete del servizio project che richiama il backend/api

--- project-angular/src/services/project.service.ts
+++ project-angular/src/services/project.service.ts
@@ -35,17 +35,29 @@ export class ProjectService{
         let params = JSON.stringify(project);
         let headers = new HttpHeaders().set('Content-Type','application/json');
 
+        // Nombre de la ruta en el backend
         return this._http.post(this.url+'save-project', params, {headers: headers});
     }
 
     getProjects(): Observable<any>{
         let headers = new HttpHeaders().set('Content-Type', 'applicaton/json');
 
+        // Nombre de la ruta en el backend
         return this._http.get(this.url+'projects', {headers: headers});
     }
+
     getDetail(id): Observable<any>{
         let headers = new HttpHeaders().set('Content-Type','application/json');
 
+        // Nombre de la ruta en el backend
         return this._http.get(this.url+'project/'+id, {headers: headers});
     }
+
+    deleteProject(id): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type','application/json');
+
+        // Nombre de la ruta en el backend
+        return this._http.delete(this.url+'delete/'+id, {headers: headers});
+    }
+
 }
