f911b43b2bc3e79712cc5c54e95faec96a1c7f61
Adrian Medina <medinalarry96@gmail.com>
2020-10-18 11:57:18 +0200

    Slider sul componente ts

--- project-angular/src/app/components/contact/contact.component.ts
+++ project-angular/src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { $ } from 'protractor';
+//import { Global } from '../../../services/global';
 
 /**
  * Variabili globali.
@@ -15,26 +15,51 @@ declare var $:any;
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  //public url: string;
+
+  constructor() {
+    //this.url= Global.url;
+   }
 
   ngOnInit(): void {
-
-//    $("header").css("background", "green");
     
-
-
     /**
      * Cambia colore con jquery.
      * bisogna declarare le variabili globali.
-     */
+    */ 
     $("#logo").click(function(e){
       e.preventDefault();
 
-        $("header").css("background", "green")
-                 .css("height","50px");
+        $("header").css("background", "rgba(9, 148, 0, 0.8)")
+                 .css("height","90px");
     });
     
+    /**
+     * Slider
+     */
+    $('.galeria').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 600
+    })
+
 
   }
+
+  /*
+  getDetail(id) {
+    this._projectService.getDetail(id).subscribe(
+      response => {
+        this.project = response.project;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+  */
+
+
+
 
 }
