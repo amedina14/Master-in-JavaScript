82608aa835b48c6e85a48b0ca1d5f76c3da999ff
Adrian Medina <medinalarry96@gmail.com>
2020-10-20 20:31:12 +0200

    ricevere nel client la data e connettere il client al socket

--- sezione-sockets/client/main.js
+++ sezione-sockets/client/main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const socket = io.connect('http://192.168.1.3:6677', {'forceNew':true});
+
+/**
+ * Recibe el emit de la conexion desde el index.js
+ * 
+ * Se ejecut el socket y trabaja lo que hay en su interno.
+ */
+socket.on("messages", (data) => {
+    console.log(data);
+    render(data);
+});
+
