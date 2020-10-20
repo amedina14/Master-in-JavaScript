0158cb6611c21a481d5b6b8e46d36a8b7e65be56
Adrian Medina <medinalarry96@gmail.com>
2020-10-20 18:34:24 +0200

    Connessione al socket e caricare static view

--- sezione-sockets/server/index.js
+++ sezione-sockets/server/index.js
@@ -19,6 +19,16 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 /**
+ * Middleware express para 
+ * carga una vista estatica por defecto.
+ * 
+ * Cargar la vista de la chat.
+ * 
+ * Para usar un MIDDLEWARE se usa USE()
+ */
+app.use(express.static('client'));
+
+/**
  * Definiendo la route.
  */
 app.get('/get', (req, res) => {
@@ -26,6 +36,17 @@ app.get('/get', (req, res) => {
 
 })
 
+/**
+ * Conexion al socket
+ * Lanzar evento connection para los clientes.
+ * La conexion es llamada y recibe las conexiones de los clientes.
+ * Detecta cada vez que un cliente se conecta.
+ * 
+ * Parametro socket lleva toda la informacion del mismo.
+ */
+io.on('connection', (socket) => {
+    console.log("El cliente con IP: "+ socket.handshake.address +" se ha conectado.");
+});
 
 /**
  * Creacion de server con express.
