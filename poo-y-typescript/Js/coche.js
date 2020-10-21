29eaf706fee9006157c3b85ad8504867a20f9866
Adrian Medina <medinalarry96@gmail.com>
2020-10-21 18:18:34 +0200

    Metodi della clase coche.ts

--- poo-y-typescript/Js/coche.js
+++ poo-y-typescript/Js/coche.js
@@ -1,11 +1,26 @@
 var Coche = /** @class */ (function () {
     function Coche() {
+        this.color = "";
+        this.modelo = "";
+        this.velocidad = 0;
     }
     Coche.prototype.getColor = function () {
         return this.color;
     };
     Coche.prototype.setColor = function (color) {
         this.color = color;
+    };
+    Coche.prototype.acelerar = function () {
+        this.velocidad++;
+    };
+    Coche.prototype.frenar = function () {
+        this.velocidad--;
+    };
+    Coche.prototype.apagar = function (velocidad) {
+        return this.velocidad = velocidad;
+    };
+    Coche.prototype.getVelocidad = function () {
+        return this.velocidad;
     };
     return Coche;
 }());
@@ -18,3 +33,10 @@ coche_tres.setColor("Verde");
 console.log("El color de coche 1 es: " + coche.getColor());
 console.log("El color de coche 2 es: " + coche_dos.getColor());
 console.log("El color de coche 3 es: " + coche_tres.getColor());
+for (var i = 0; i < 3; i++) {
+    coche.acelerar();
+    console.log("Acelerando. La velocidad del coche es:", coche.getVelocidad());
+}
+coche.frenar();
+console.log("La velocidad del coche es:", coche.getVelocidad());
+console.log("Fin de viaje. Coche apagado. Velocidad: ", coche.apagar(0));
--- poo-y-typescript/Js/coche.ts
+++ poo-y-typescript/Js/coche.ts
@@ -3,12 +3,31 @@ class Coche{
     public modelo: string;
     public velocidad: number;
 
+    constructor(){
+        this.color = "";
+        this.modelo = "";
+        this.velocidad = 0;
+    }
+
     public getColor(){
         return this.color;
     }
 
     public setColor(color: string){
         this.color = color;
+    }
+
+    public acelerar(){
+        this.velocidad++;
+    }
+    public frenar(){
+        this.velocidad--;
+    }
+    public apagar(velocidad: number){
+        return this.velocidad = velocidad;
+    }
+    public getVelocidad():number{
+        return this.velocidad;
     }
 }
 
@@ -19,6 +38,17 @@ var coche_tres = new Coche();
 coche.setColor("Rojo");
 coche_dos.setColor("Azul");
 coche_tres.setColor("Verde");
+
 console.log("El color de coche 1 es: "+ coche.getColor());
 console.log("El color de coche 2 es: "+ coche_dos.getColor());
 console.log("El color de coche 3 es: "+ coche_tres.getColor());
+
+for(let i = 0; i<3; i++){
+    coche.acelerar();
+    console.log("Acelerando. La velocidad del coche es:", coche.getVelocidad());
+}
+
+coche.frenar();
+console.log("La velocidad del coche es:", coche.getVelocidad());
+
+console.log("Fin de viaje. Coche apagado. Velocidad: ", coche.apagar(0));
