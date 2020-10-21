a04b6aedcad59078ac79dd3d13dfaa396a114ab2
Adrian Medina <medinalarry96@gmail.com>
2020-10-21 12:30:54 +0200

    Funzione di renderizzare i messaggi sulla view

--- sezione-sockets/client/main.js
+++ sezione-sockets/client/main.js
@@ -5,10 +5,26 @@ const socket = io.connect('http://192.168.1.3:6677', {'forceNew':true});
 /**
  * Recibe el emit de la conexion desde el index.js
  * 
- * Se ejecut el socket y trabaja lo que hay en su interno.
+ * Se ejecuta el socket y trabaja lo que hay en su interno.
+ * El socket envia el evento "messages"
+ * Cuando lleguen los mensages, se ejecuta el socket.on
+ * y renderiza los datos.
  */
 socket.on("messages", (data) => {
     console.log(data);
     render(data);
 });
 
+function render(data){
+    let html = data.map((message, index) => {
+        return (`
+            <div class="message">
+            <b>${message.nickName}</b> says:
+            <p>${message.text}</p>
+            </div>
+        `);
+    }).join(' ');
+
+    // Stampa il messaggio dentro il html
+    document.getElementById('messages').innerHTML = html;
+}
